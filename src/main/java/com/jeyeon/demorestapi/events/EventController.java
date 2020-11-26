9ad62578970609c3dfda1323f4ae427f3cd0652e
JeeEun Yon <jeyeon@payletter.com>
2020-11-26 17:38:02 +0900

    EventController set Body test successful

--- src/main/java/com/jeyeon/demorestapi/events/EventController.java
+++ src/main/java/com/jeyeon/demorestapi/events/EventController.java
@@ -1,2 +1,28 @@
-package com.jeyeon.demorestapi.events;public class EventController {
+package com.jeyeon.demorestapi.events;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.MediaTypes.*;
+import static org.springframework.hateoas.server.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.ControllerLinkBuilder.methodOn;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = HAL_JSON_VALUE)
+public class EventController {
+
+    @PostMapping//("/api/events")
+    public ResponseEntity createEvent(@RequestBody Event event){
+        //URI createdUri = linkTo(methodOn(EventController.class).createEvent(null)).slash("{id}").toUri();
+        URI createdUri = linkTo(EventController.class).slash("{id}").toUri();
+        event.setId(10);
+        //return ResponseEntity.created(createdUri).build();
+        return ResponseEntity.created(createdUri).body(event);
+    }
 }
--- src/test/java/com/jeyeon/demorestapi/events/EventControllerTests.java
+++ src/test/java/com/jeyeon/demorestapi/events/EventControllerTests.java
@@ -1,2 +1,54 @@
-package com.jeyeon.demorestapi.events;public class EventControllerTests {
+package com.jeyeon.demorestapi.events;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+//@WebAppConfiguration
+public class EventControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020,11,26,16,52))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020,11,27,16,52))
+                .beginEventDateTime(LocalDateTime.of(2020,11,28,16,52))
+                .endEventDateTime(LocalDateTime.of(2020,11,29,16,52))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("역삼")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
+    }
 }
