cc1dca5a2899762c522fee84b03dee75af329ef2
JeeEun Yon <jeyeon@payletter.com>
2020-11-30 11:46:27 +0900

    Add custom description annotation

---
+++ src/main/java/com/jeyeon/demorestapi/common/TestDescription.java
@@ -0,0 +1,13 @@
+package com.jeyeon.demorestapi.common;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.SOURCE)
+public @interface TestDescription {
+
+    String value();
+}
--- src/test/java/com/jeyeon/demorestapi/events/EventControllerTests.java
+++ src/test/java/com/jeyeon/demorestapi/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package com.jeyeon.demorestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jeyeon.demorestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,6 +40,7 @@ public class EventControllerTests {
 //    EventRepository eventRepository;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         //Event event = Event.builder()
         EventDto event = EventDto.builder()
@@ -76,6 +78,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -107,6 +110,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -117,6 +121,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
