b740a09b6325295bff170503424c6a32ee511517
JeeEun Yon <jeyeon@payletter.com>
2020-12-04 11:40:32 +0900

    SpringBoot Hateos applied (automatically used VS Spring)

--- src/main/java/com/jeyeon/demorestapi/events/EventResource.java
+++ src/main/java/com/jeyeon/demorestapi/events/EventResource.java
@@ -1,9 +1,12 @@
 package com.jeyeon.demorestapi.events;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
+import static org.springframework.hateoas.server.mvc.ControllerLinkBuilder.linkTo;
 
-public class EventResource extends RepresentationModel {    //ResourceSupport deprecated
+/*public class EventResource extends RepresentationModel {    //ResourceSupport deprecated
 
     @JsonUnwrapped  //JsonSerialize할 때 event 객체로 묶지 않음
     private Event event;
@@ -14,5 +17,14 @@ public class EventResource extends RepresentationModel {    //ResourceSupport de
 
     public Event getEvent() {
         return event;
+    }
+}*/
+
+//Automatically JsonUnwrapped used.
+public class EventResource extends EntityModel<Event> {    //Resource deprecated
+
+    public EventResource(Event event, Link... links) {
+        super(event, links);
+        add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
     }
 }
